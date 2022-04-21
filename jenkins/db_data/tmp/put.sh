6a15a4ff7a9617a91508abd4c02d0a504773c8d7
AlexBasa <alexsimple@gmail.com>
2022-04-21 23:29:45 +0300

    Populating mysql database by shell script

---
+++ jenkins/db_data/tmp/put.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+counter=0
+
+while [ $counter -lt 50 ]; do
+    name=$(nl people.txt | grep -w $counter | awk '{print$2}' | awk -F ',' '{print$1}')
+    lastname=$(nl people.txt | grep -w $counter | awk '{print$2}' | awk -F ',' '{print$2}')
+    age=$(shuf -i 5-50 -n 1)
+    let counter=counter+1
+    mysql -u root -p1234 people -e "insert into register values ($counter, '$name', '$lastname, $age)"
+    echo "$counter, $name $lastname, $age was correctly imported"
+done
